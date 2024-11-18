ad3a98c785e25560f4d31e93ae6b85c473fab216
Timofey Ivlev <timofejivlev923@gmail.com>
2024-11-18 17:36:10 +0300

    update

--- todo_list_website/main/views.py
+++ todo_list_website/main/views.py
@@ -1,11 +1,13 @@
 
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView , FormView
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
 from .models import Task
+from django.contrib.auth import login
 
 class CustomLoginView(LoginView):
     template_name = 'main/login.html'
@@ -21,12 +23,30 @@ class TaskList(LoginRequiredMixin, ListView):
     context_object_name = 'tasks'
 
 
+    def get_context_data(self,  **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['count'] = context['tasks'].filter(complete = False).count
+        return context
+
+
 class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'main/list.html'
 
 
+
+class RegisterPage(FormView):
+    template_name = 'main/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request,user)
+        return super(RegisterPage , self).form_valid(form)
 class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
     fields = ['title', 'description', 'complete']  # Указывайте только нужные поля
