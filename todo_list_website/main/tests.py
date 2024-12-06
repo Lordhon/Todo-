54583bcfb4b9f18531e9ab7461c0fd4a5cace974
Timofey Ivlev <timofejivlev923@gmail.com>
2024-12-06 19:28:20 +0300

    +test

--- todo_list_website/main/tests.py
+++ todo_list_website/main/tests.py
@@ -1,6 +1,7 @@
 
 from django.test import TestCase
 from django.contrib.auth.models import User
+from .models import Task
 
 class TestTasks(TestCase):
     def setUp(self):
@@ -10,3 +11,13 @@ class TestTasks(TestCase):
         self.client.login(username='testuser', password='password123')
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
+
+class TestTasksBD(TestCase):
+    def setUp(self):
+        user = User.objects.create_user(username='user1', password='password123')
+        Task.objects.create(user=user, title="qwe", description="aszx", complete=False)
+
+    def test_Task(self):
+        task = Task.objects.get(title="qwe")
+        self.assertEqual(str(task), "qwe")
+
